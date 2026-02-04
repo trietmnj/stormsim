93dc72e1cd553654d3395ce83ae04d9654fea541
FGM <fagarcia93@@gmail.com>
2026-02-03 23:52:25 -0600

    deleting old files

--- conversion/lifecycle-generation/lcgen/__init__.py
+++
@@ -1,5 +0,0 @@
-# conversion/lifecycle-generation/lcgen/__init__.py
-from . import sampling
-from . import load
-from . import validation
-from . import utils
--- conversion/noaa-requests/noaa-py/noaapy/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import station_list
